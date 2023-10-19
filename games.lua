b869cb6e8ea4da1fb3fdb6b44c58fc8cf371e07e
LUtayaQQ <148413317+LUtayaQQ@users.noreply.github.com>
2023-10-19 11:10:20 +0700

    Add files via upload

---
+++ games.lua
@@ -0,0 +1,22 @@
+gamemap().TQ = {
+    -------------------------------------Treasure-Quest-------------------------------------------------------
+    [2960777560] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
+    [2978518061] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest"
+    ----------------------------------------------------------------------------------------------------------
+}
+getgenv().LRW1 = {
+    --------------------------------------Legends Re:Written 1------------------------------------------------
+    [7816227995] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-1"
+    ----------------------------------------------------------------------------------------------------------
+}
+getgenv().LRW2 = {
+    --------------------------------------Legends Re:Written 2-------------------------------------------------
+    [11650685514] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-2"
+    ----------------------------------------------------------------------------------------------------------
+}
+getgenv().LRWRaid = {
+    --------------------------------------Legends Re:Written Raid----------------------------------------------
+    [8793520828] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
+    [10127152911] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid"
+    -----------------------------------------------------------------------------------------------------------
+}
